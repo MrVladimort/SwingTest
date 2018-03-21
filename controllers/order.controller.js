d887ca8d603ccacae03a45f3a0a7ccb14bc1e94e
Vladyslav <mrvladimort@gmail.com>
2018-03-21 19:17:44 +0100

    update

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,25 +1,21 @@
 const orderService = require('../services/order.service');
 const HttpError = require('../error');
-const logger = require('../logger');
 const { validationResult } = require('express-validator/check');
 
 module.exports.createOrder = async (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        throw new HttpError(422, JSON.stringify(errors.mapped()));
-    }
+    if (!errors.isEmpty()) throw new HttpError(422, JSON.stringify(errors.mapped()));
+
 
     const order = orderService.composeOrder(req.body);
-    await orderService.createOrder(order);
+    // await orderService.createOrder(order);
 
     res.json(order);
 };
 
 module.exports.getOrders = async (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        throw new HttpError(422, JSON.stringify(errors.mapped()));
-    }
+    if (!errors.isEmpty()) throw new HttpError(422, JSON.stringify(errors.mapped()));
 
     const orders = await orderService.gerOrders();
 
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,9 +1,8 @@
 "use strict";
 const router = require('express-promise-router')();
 const orderCtrl = require('../controllers/order.controller');
-const orderValidator = require('../validators/order.validator');
 
-router.post('/', orderValidator.validateCreateOrder, orderCtrl.createOrder);
+router.post('/', orderCtrl.createOrder);
 router.get('/', orderCtrl.getOrders);
 
 module.exports = router;
